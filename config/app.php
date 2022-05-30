0a367fe873b97f835fa51387506916ce2291f851
testtest <yongtyao@163.com>
2022-05-30 10:07:51 +0800

    fix:domain_bind

--- config/app.php
+++ config/app.php
@@ -21,7 +21,7 @@ return [
     'app_map'          => [],
     // 域名绑定（自动多应用模式有效）
     'domain_bind'      => [
-        'salvage.stgui.com'=>'program',
+        'stgui.com'=>'program',
     ],
     // 禁止URL访问的应用列表（自动多应用模式有效）
     'deny_app_list'    => [],
