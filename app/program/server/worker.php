f45ba360b39e23a1a4e8bbdcf9facb27419be454
testtest <yongtyao@163.com>
2022-12-30 18:19:05 +0800

    add alarm

--- app/program/server/worker.php
+++ app/program/server/worker.php
@@ -8,6 +8,7 @@ use app\program\controller\IndexController;
 use app\program\model\Alarm;
 use app\program\model\Message;
 use app\program\model\User;
+use app\program\model\Facility;
 use app\program\service\CommonService;
 use think\Cache;
 use think\facade\Db;
@@ -26,7 +27,7 @@ class worker extends Server
     protected $protocol = 'tcp';
     protected $port = '999' ;
     protected $host = '0.0.0.0';
-
+    
 
     public function onWorkerReload($worker) {
 
@@ -95,16 +96,23 @@ class worker extends Server
         // 16 进制 395b6419， 字符串为 579110025
         $ID = $data['ID'];
         $server->writeWorkmanLog("消息发送方的设备ID=".$ID);
+        $msgAlarm = dechex(01).dechex(00).dechex(01).dechex(01).dechex(01).dechex(01).("ALARM");
         if ($ID == "579110025") {
             // 读取缓存
             if (\think\facade\Cache::has('alarm')) {
-                $msgAlarm = dechex(01).dechex(00).dechex(01).dechex(01).dechex(01).dechex(01).("ALARM");
-
                 $server->writeWorkmanLog("触发一次远程报警器");
                 $connection->send($msgAlarm);
                 \think\facade\Cache::delete('alarm');
                 return;
             }
+        }
+
+        $facility = Facility::where(['facility_id' => $ID])->find();
+        if ($facility != null && $facility->alarm_status == Facility::ALARM_STATUS_1) {
+            $server->writeWorkmanLog("触发一次手动触发报警ID=".$ID);
+            // 触发一次报警命令
+            $this->alarm($data);
+            $connection->send($msgAlarm);
         }
 
         //警报记录
@@ -138,29 +146,8 @@ class worker extends Server
                             // 向管理员与值班室发送数据
                             $server->writeWorkmanLog("收到一次报警消息，将进行报警处理，设备 ID=".$data['ID']);
 
-                            $alarm_decode_address = $data['ID'];
-                            if ($data['ID'] == "1602100") {
-                                $alarm_decode_address = "15号平台A侧";
-                            } else if ($data['ID'] == "1772100" || $data['ID'] == "1782100") {
-                                $alarm_decode_address = "15号平台B侧";
-                            } else if ($data['ID'] == "1922100") {
-                                $alarm_decode_address = "15号平台C侧";
-                            } else if ($data['ID'] == "2092100" || $data['ID'] == "2102100") {
-                                $alarm_decode_address = "15号平台D侧";
-                            }
-
-                            $users = User::where('role', 'in', User::ROLE_40.','.User::ROLE_10)->where(['status' => 1])->select();
-                            foreach ($users as $user) {
-                                // 发送小程序弹窗告警
-                                if ($user->openid) {
-                                    $server->sendMessageToUser($user->openid, '警报', $alarm_decode_address);
-                                }
-                                // 发送短信        
-                                if ($user->tel) {
-                                    // $server->sms($user->tel,$address);
-                                    $smsResp = $server->sendSMS($user->tel, $alarm_decode_address);
-                                }
-                            }
+                            // 触发一次报警命令
+                            $this->alarm($data);
 
                             $msgAlarm = dechex(01).dechex(00).dechex(01).dechex(01).dechex(01).dechex(01).("ALARM");
                             $server->writeWorkmanLog("触发一次本地报警器，设备 ID=".$data['ID']);
@@ -178,5 +165,32 @@ class worker extends Server
         $res = substr($data['data'], 3, 8);
         $res .= dechex(01).dechex(00).dechex(01).dechex(01).dechex(01).dechex(01);
         $connection->send($res);
+    }
+
+    public function alarm($data) {
+        $alarm_decode_address = $data['ID'];
+        if ($data['ID'] == "1602100") {
+            $alarm_decode_address = "15号平台A侧";
+        } else if ($data['ID'] == "1772100" || $data['ID'] == "1782100") {
+            $alarm_decode_address = "15号平台B侧";
+        } else if ($data['ID'] == "1922100") {
+            $alarm_decode_address = "15号平台C侧";
+        } else if ($data['ID'] == "2092100" || $data['ID'] == "2102100") {
+            $alarm_decode_address = "15号平台D侧";
+        }
+
+        $server = new CommonService();
+        $users = User::where('role', 'in', User::ROLE_40.','.User::ROLE_10)->where(['status' => 1])->select();
+        foreach ($users as $user) {
+            // 发送小程序弹窗告警
+            if ($user->openid) {
+                $server->sendMessageToUser($user->openid, '警报', $alarm_decode_address);
+            }
+            // 发送短信        
+            if ($user->tel) {
+                // $server->sms($user->tel,$address);
+                $smsResp = $server->sendSMS($user->tel, $alarm_decode_address);
+            }
+        }
     }
 }
