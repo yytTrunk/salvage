32b9d75753d11efb2f161f3f455cc5233b2fd8b5
yyt <yongtyao@163.com>
2023-08-20 21:29:17 +0800

    2 socketserver

--- config/worker_server.php
+++ config/worker_server.php
@@ -16,12 +16,10 @@ return [
     // 扩展自身需要的配置
     'protocol'       => 'tcp', // 协议 支持 tcp udp unix http websocket text
     'host'           => '0.0.0.0', // 监听地址
-    'port'           => 998, // 监听端口
+    'port'           => 999, // 监听端口
     'socket'         => '', // 完整监听地址
     'context'        => [], // socket 上下文选项
-    'worker_class'   => ['app\program\server\workerCamera'], // 自定义Workerman服务类名 支持数组定义多个服务
-
-    // 'worker_class'   => ['app\program\server\worker','app\program\server\workerCamera'], // 自定义Workerman服务类名 支持数组定义多个服务
+    'worker_class'   => ['app\program\server\worker','app\program\server\workerCamera'], // 自定义Workerman服务类名 支持数组定义多个服务
 
     // 支持workerman的所有配置参数
     'name'           => 'thinkphp',
