bb7bc24b638cb925c8dc883d2fed7c8bb92ab38c
summerdha <1204414468@qq.com>
2024-01-07 11:47:30 +0800

    fix

--- app/program/server/workerHCLM.php
+++ app/program/server/workerHCLM.php
@@ -203,13 +203,13 @@ class workerHCLM extends Server
         $server = new CommonService();
         $result = $server->send_post("https://open.ys7.com/api/lapp/token/get?appKey=eec1f9d9ac8a48ea99c59b889bc2291c&appSecret=9c0f0c0dd74365a4f8e5e152d8c06fc9", "");
         var_dump($result);
-        if ($result->code == 200) {
+        if ($result['code'] == 200) {
             var_dump($result->data);
             // 抓拍
-            // $data = $json_ret->data;
-            // $access_token = $data -> accessToken;
+            $data = $result['data'];
+            $access_token = $data['accessToken'];
 
-            // $server->writeWorkmanLog("获取token成功 = ".$access_token);
+            $server->writeWorkmanLog("获取token成功 = ".$access_token);
         }
     }
 
